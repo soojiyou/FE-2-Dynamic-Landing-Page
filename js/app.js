4269a51dbd4e189003ce9e9c20b77fe5b534e2f0
soojiyou <sooji.you@wsu.edu>
2022-07-30 20:53:07 -0700

    7/30

--- js/app.js
+++ js/app.js
@@ -1,221 +1,66 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- *
-*/
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- *
-*/
-
-// function anchorlink(linksource) {
-
-//     for (i = 0; i <= linksource.length - 1; i++) {
-//         const li = document.getElementById(`list${i + 1}`);
-//         //const anchor = document.createElement('a');
-//         li.setAttribute('href', `${linksource[i]}`);
-//         //li.append(anchor);
-//         //li.innerHTML = `<a href="${linksource[i]}">${arrli[i]}</a>`;
-//     }
-// }
-
-// function makeli(arrli) {
-//     // const container = document.getElementsByClassName("navbar__menu");
-//     const ul = document.getElementById("navbar__list");
-
-//     for (i = 0; i <= arrli.length - 1; i++) {
-//         const li = document.createElement('li');
-
-//         li.innerHTML = arrli[i];
-//         li.setAttribute('id', `list${i + 1}`)
-//         ul.appendChild(li);
-//     }
-//     // container.appendChild(ul);
-
-// }
-
-function makeliandanchorlink(arrli, linksource) {
+function makeLiandAnchorLink(arrli, linksource) {
     const ul = document.getElementById("navbar__list");
 
     for (i = 0; i <= arrli.length - 1; i++) {
-        const li = document.createElement('li');
-        li.setAttribute('data-section', `${i}`);
+        let li = document.createElement('li');
+        //li.setAttribute('data-section', `${i + 1}`);
+        //li.innerHTML = `${arrli[i]}`;
         li.innerHTML = `<a href = "${linksource[i]}"> ${arrli[i]}</a>`;
-        li.setAttribute('id', `list${i + 1}`);
+        //li.setAttribute('id', `list${i + 1}`);
+        li.setAttribute('class', "nav-item");
         ul.appendChild(li);
-
+        let lia = document.getElementsByTagName('a')[i];
+        lia.setAttribute('class', `nav-link${i + 1}`);
     }
-
-    li0 = document.getElementsByTagName('a')[0];
-    li0.setAttribute('class', "active");
+    // let li0 = document.getElementsByTagName('a')[0];
+    //li0.append('class', "active");
 }
 
-/////////////arrli = link///////
-// function checkVisibleSection() {
 
-//     var minor = window.innerHeight,
-//         section = null;
+function selectClass(className) {
+    return document.querySelector(`.${className}`);
+}
 
-//     //---Select the section closest to the top
-//     [].forEach.call(sections, function (item) {
-
-//         var offset = item.getBoundingClientRect();
-
-//         if (Math.abs(offset.top) < minor) {
-
-//             minor = Math.abs(offset.top);
-
-//             section = item;
-
-//         }
-
-//     });
-//     if (section) {
-//         let index = section.data - section;
-//         let link = nav.querySelector("li[data-section='" + index + "'] a");
-//         if (!link.classList.contains("active")) {
-
-//             //---Remove the active class
-//             nav.querySelector("a.active").classList.remove("active");
-
-//             //---Add the active class
-//             link.classList.add("active");
-//         }
-
-//     }
-
-// }
+function selectId(idName) {
+    return document.querySelector(`#${idName}`);
+}
+function callback(target_sections) {
+    target_sections.forEach((target_section) => {
+        if (target_section.isIntersecting) {
+            const navlink = navlinks[target_section.target.id];
+            navlink.classList.add('active');
+            Object.values(navlinks).forEach((link) => {
+                if (link != navlink) {
+                    link.classList.remove('active');
+                }
+            });
+        }
+    });
+}
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- *
-*/
 // build the nav
 
-makeliandanchorlink(['Pomeranian', 'Appearance', 'Behavior', 'Health', 'History'], ['#', '#', '#', '#', '#'])
+makeLiandAnchorLink(['Pomeranian', 'Appearance', 'Behavior', 'Health', 'History'], ['#section1', '#section2', '#section3', '#section4', '#section5'])
 
 
 // Add class 'active' to section when near top of viewport
 
 
-// var nav = document.getElementById("navbar__list"),
-//     sections = document.querySelectorAll(".landing__container"),
-//     delay = null;
+const sections = [selectId('section1'), selectId('section2'), selectId('section3'), selectId('section4'), selectId('section5')];
 
-// //---Scroll logic
-// document.addEventListener("scroll", function () {
+const navlinks = { section1: selectClass('nav-link1'), section2: selectClass('nav-link2'), section3: selectClass('nav-link3'), section4: selectClass('nav-link4'), section5: selectClass('nav-link5') };
 
-//     if (!isNaN(delay)) { clearTimeout(delay); }
-
-//     delay = setTimeout(checkVisibleSection, 100);
-
-// });
-
-// //---Check the visible section
-// function checkVisibleSection() {
-
-//     var minor = window.innerHeight,
-//         section = null;
-
-//     //---Select the section closest to the top
-//     [].forEach.call(sections, function (item) {
-
-//         var offset = item.getBoundingClientRect();
-
-//         if (Math.abs(offset.top) < minor) {
-
-//             minor = Math.abs(offset.top);
-
-//             section = item;
-
-//         }
-
-//     });
-
-//     //---If the section exists
-//     if (section) {
-
-//         var index = section.dataset.section,
-//             link = nav.querySelector("[li[data-section='" + index + "'] a");
-
-//         //---If the link is not already active
-//         if (!link.classList.contains("active")) {
-
-//             //---Remove the active class
-//             nav.querySelector("a.active").classList.remove("active");
-
-//             //---Add the active class
-//             link.classList.add("active");
-
-//         }
-
-//     }
-
-// }
-
-// //---Click on buttons
-// nav.addEventListener("click", function (evt) {
-
-//     evt.preventDefault();
-
-//     var link = evt.target;
-
-//     if (link.nodeName.toLowerCase() === "a") {
-
-//         var section = link.parentNode.dataset.section;
-
-//         //---Remove the class of the active link
-//         nav.querySelector("a.active").classList.remove("active");
-
-//         //---Active the link
-//         link.classList.add("active");
-
-//         //---Scroll to the section
-//         document.getElementById("section" + section).scrollIntoView();
-
-//         document.body.scrollTop -= 30;
-
-//     }
-
-// });
+const options = {
+    root: null,
+    rootMargin: '0px',
+    thresholds: 1.0
+};
 
 
+let observer = new IntersectionObserver(callback, options);
 
-// Scroll to anchor ID using scrollTO event
+sections.forEach((section) => observer.observe(section));
 
-
-/**
- * End Main Functions
- * Begin Events
- *
-*/
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
